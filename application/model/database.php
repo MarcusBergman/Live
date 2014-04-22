9e5cee90f59e5566e912975d998fe46dea0f373d
Bragge <trygglimurr@gmail.com>
2014-04-22 11:56:39 +0200

    random string

--- application/model/database.php
+++ application/model/database.php
@@ -5,7 +5,14 @@ $db = dba_open("model/database.db4", "c", "db4") or die( "Couldn't open Database
 
 function newTest()
 {
-  return 57; 
+  function generateRandomString($length = 5) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, strlen($characters) - 1)];
+    }
+}
+      return $randomString; 
 }
 
 ?>
