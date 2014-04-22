46f15bf488168f14169214667dc3bbec31d4e623
Bragge <trygglimurr@gmail.com>
2014-04-22 13:06:22 +0200

    positionering och storlek css

--- application/model/database.php
+++ application/model/database.php
@@ -5,14 +5,14 @@ $db = dba_open("model/database.db4", "c", "db4") or die( "Couldn't open Database
 
 function newTest()
 {
-  function generateRandomString($length = 5) {
+  function randomString($length = 5) {
     $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $randomString = '';
+    $string = '';
     for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        $string .= $characters[rand(0, strlen($characters) - 1)];
     }
-    return $randomString;
-}
+    return $string;
+  }
 
   return 57; 
 }
