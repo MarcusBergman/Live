1cad17ed53b9e5f3bb131ad90f1da12a2c3f1a51
Bragge <trygglimurr@gmail.com>
2014-04-22 16:17:51 +0200

    newTest funktion, do do, test array
    osäker på vart header location skulle vara

--- application/model/database.php
+++ application/model/database.php
@@ -1,20 +1,49 @@
 <?php
 
+  $db = dba_open("model/database.db4", "c", "db4") or die( "Couldn't open Database" ); 
 
-$db = dba_open("model/database.db4", "c", "db4") or die( "Couldn't open Database" ); 
-
-function newTest()
-{
-  function randomString($length = 5) {
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $string = '';
-    for ($i = 0; $i < $length; $i++) {
+  function newTest()
+  {
+    function randomString($length = 5) {
+      $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      $string = '';
+      for ($i = 0; $i < $length; $i++) {
         $string .= $characters[rand(0, strlen($characters) - 1)];
+      }
+      return $string;
     }
-    return $string;
-  }
 
-  return 57; 
+    do
+    {
+    $publicID = randomString();
+    while (dba_fetch($publicID, $db));
+    }
+  
+    do
+    {
+    $privateID = randomString();
+    $keyIsNotUnique = "false";
+    $key = dba_firstkey($db);
+    while($key != NULL)
+    {
+      $test = json_decode(dba_fetch($key, $db), true);
+      if($test['privateID'] == $privateID)
+        $keyIsNotUnique = "true";
+      
+	  $key = dba_nextkey($db);
+    }
+    while (! $keyIsNotUnique);
+  }
 }
 
+  $test = array();
+
+  $test['privateID'] = $privateID;
+  $test['title'] = "En titel";
+  $test['images'] = array();
+
+  dba_insert($publicID, json_encode($test), $db);
+
+  header("Location: http://august.friskola.nu/redigera/".$publicID);
+  
 ?>
