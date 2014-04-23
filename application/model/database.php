9e3e89cffa2bbf4a5b4ff9cad2cccbcd7a6de1b8
Mikael Tylmad <mikael@tylmad.com>
2014-04-23 10:00:39 +0200

    Update database.php

--- application/model/database.php
+++ application/model/database.php
@@ -4,46 +4,52 @@
 
   function newTest()
   {
-    function randomString($length = 5) {
-      $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    function randomString($length = 5) 
+    {
+      $characters = 'abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ';
       $string = '';
-      for ($i = 0; $i < $length; $i++) {
+      for ($i = 0; $i < $length; $i++) 
         $string .= $characters[rand(0, strlen($characters) - 1)];
-      }
+      
       return $string;
     }
 
+    // Leta efter ett ledigt publikt ID.
     do
     {
-    $publicID = randomString();
-    while (dba_fetch($publicID, $db));
+      $publicID = randomString();
     }
-  
+    while (dba_fetch($publicID, $db));
+    
+    // Leta efter ett ledigt privat ID.
     do
     {
-    $privateID = randomString();
-    $keyIsNotUnique = "false";
-    $key = dba_firstkey($db);
-    while($key != NULL)
-    {
-      $test = json_decode(dba_fetch($key, $db), true);
-      if($test['privateID'] == $privateID)
-        $keyIsNotUnique = "true";
+      $privateID = randomString();
+      $keyIsNotUnique = "false";
+      $key = dba_firstkey($db);
+
+      while($key != NULL)
+      {
+        $test = json_decode(dba_fetch($key, $db), true);
+        if($test['privateID'] == $privateID)
+          $keyIsNotUnique = "true";
       
-	  $key = dba_nextkey($db);
+	$key = dba_nextkey($db);
+      }
     }
     while (! $keyIsNotUnique);
+  
+    // Nu har vi hittat både privat och publikt ID. Dags att skapa testet!
+    $test = array();
+
+    $test['privateID'] = $privateID;
+    $test['title'] = "En titel";
+    $test['images'] = array();
+
+    dba_insert($publicID, json_encode($test), $db);
+    
+    return $publicID;
   }
-}
-
-  $test = array();
-
-  $test['privateID'] = $privateID;
-  $test['title'] = "En titel";
-  $test['images'] = array();
-
-  dba_insert($publicID, json_encode($test), $db);
-
-  header("Location: http://august.friskola.nu/redigera/".$publicID);
+  
   
 ?>
