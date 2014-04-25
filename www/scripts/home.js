a9b9d064ce2571862e97b5d9c151add6c63fe097
Bragge <trygglimurr@gmail.com>
2014-04-25 13:43:34 +0200

    home.js

---
+++ www/scripts/home.js
@@ -0,0 +1,24 @@
+var polyfilter_scriptpath = '/scripts/filter_polyfill/'; 
+  blur = function()
+  {
+    $("#popup").css("top", "30%");
+    $("#popup").animate({opacity: "1"},500);
+	  
+    $("#main").animate({ pixels: 8 },
+    {
+      step: function(now,fx) 
+      {
+        $(this).css('polyfilter','blur(' + Math.round(now) + 'px)'); 
+      },
+        duration: 500
+    },'swing');
+  };
+		
+  $("#starta").click(blur);
+  
+  $("#kryss").click(function()
+  {
+    $("#popup").css("top","-100%");
+    $("#main").css("polyfilter",'blur(0px)');
+  });
+  
