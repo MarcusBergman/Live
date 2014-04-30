dd8e019acf767f5f3ee3edd6fe1298c443ab50e7
Mikael Tylmad <mikael@tylmad.com>
2014-04-30 10:50:23 +0200

    Create swapImages.php

---
+++ application/controllers/swapImages.php
@@ -0,0 +1,15 @@
+<?php
+
+  $privateID = $matches[1];
+  $firstImage = $matches[2];
+  $secondImage = $matches[3];
+  
+  $test = getTestByPrivateID($privateID);
+  
+  $tempImage                    = $test['images'][$firstImage];
+  $test['images'][$firstImage]  = $test['images'][$secondImage];
+  $test['images'][$secondImage] = $tempImage;
+  
+  updateTestByPrivateID($privateID, $test);
+
+?>
