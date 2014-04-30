e2896c8fc7276163171da3e0173866bc75e44e31
Bragge <trygglimurr@gmail.com>
2014-04-30 13:16:28 +0200

    html istället för append

--- www/scripts/run.js
+++ www/scripts/run.js
@@ -59,7 +59,7 @@ $("input[type=submit]").click(function(){
     else
     {
       $("#"+question).css("outline", "2px dashed green");
-	  $("#"+question).append('<img src="/images/bock.png" class="bock" />')
+	  $("#"+question).html('<img src="/images/bock.png" class="bock" />')
     }	
  		
 	if (allCorrect)
