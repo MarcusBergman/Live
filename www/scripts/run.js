4476ad37f35abe90784d6a9a3e65bc9935a09a7f
Bragge <trygglimurr@gmail.com>
2014-05-05 13:56:05 +0200

    dragbild

--- www/scripts/run.js
+++ www/scripts/run.js
@@ -31,7 +31,7 @@ $(".dragbild").css("width", bredd +"%");
 var image = $("img");
 
 if (image.width < image.height)
-  image.css({"height": $(".svar").height(),"margin-left": "auto", "margin-right": "auto"});    
+  image.css({"height": $(".dragbild").height(),"margin-left": "auto", "margin-right": "auto"});    
   
 $("img").load(function(){
   var hojd = $(".dragbild").height();
