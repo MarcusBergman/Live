673f73c61514ac96fa40a13a4782cb7eaea01030
Bragge <trygglimurr@gmail.com>
2014-05-05 11:21:16 +0200

    drag

--- www/scripts/run.js
+++ www/scripts/run.js
@@ -2,7 +2,6 @@ window.onorientationchange = function() { location.reload() };
 
 for (var i=1; i<=bilder.length; i++){
   $('#top').append('<div class="svar" id="svar' +i+ '"></div>');
-  $('#bilder').append('<div id="drag'+i+'" class="dragbild"></div>');
 }
 	
 function shuffleArray(array) 
@@ -19,7 +18,7 @@ function shuffleArray(array)
 var blandadeBilder = shuffleArray(bilder);
 
 for (var i=0; i<blandadeBilder.length; i++){
-	
+  $('#bilder').append('<div id="drag'+i+'" class="dragbild"></div>');	
   $('#drag'+i).append('<img src="' +bilder[i].file+ '" id="' +bilder[i].id+'" />');
 
 }
