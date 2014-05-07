283e72417d2f97a0dcfbff36603a75cd0f68d4ec
Mikael Tylmad <mikael@tylmad.com>
2014-05-07 11:15:51 +0200

    Update run.js

--- www/scripts/run.js
+++ www/scripts/run.js
@@ -24,8 +24,9 @@ for (var i=0; i<blandadeBilder.length; i++){
   
 var bredd =  84/bilder.length;
 
-$(".svar").css("width", bredd +"%").css("height", bredd +"%");
-$(".dragbild").css("width", bredd +"%").css("height", bredd +"%");
+$(".svar").css({"width": bredd +"%"});
+$(".svar").css({"height": $('.svar').width()});
+$(".dragbild").css({"width": bredd +"%", "height": $('.svar').width()});
   
 for (var i=1; i<=bilder.length; i++){
 
