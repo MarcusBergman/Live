8819ccdf2c93de46f1cf10bb20b364270e55f01a
Bragge <trygglimurr@gmail.com>
2014-05-08 13:57:29 +0200

    containment document

--- www/scripts/run.js
+++ www/scripts/run.js
@@ -29,7 +29,7 @@ $(".svar").css("height", $('.svar').width());
 $(".dragbild").css({"width": $('.svar').width(), "height": $('.svar').width()});
 	
 $(function() {
-  $( ".dragbild" ).draggable({ snap: ".svar", snapMode: "inner", containment: "#bakgrund"});
+  $( ".dragbild" ).draggable({ snap: ".svar", snapMode: "inner", containment: "document"});
     $( ".svar" ).droppable({
       drop: function( event, ui )
 	  {
