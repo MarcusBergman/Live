711605a02515b75c895eaf912390e8641ed09184
Bragge <trygglimurr@gmail.com>
2014-05-09 13:53:08 +0200

    answers.question

--- www/scripts/run.js
+++ www/scripts/run.js
@@ -56,7 +56,7 @@ $("input[type=submit]").click(function(){
     else
     {
 	  $("#"+question).html('<img src="/images/gronbock.png" class="bock" />')
-	  $("#"+answers[question]).draggable('disable');
+	  $("#"+answers.question).draggable('disable');
     }	
  		
 	if (allCorrect)
