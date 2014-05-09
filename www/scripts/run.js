c650e43d33ba35da50af3189baddfb968bbcac3e
Bragge <trygglimurr@gmail.com>
2014-05-09 13:08:08 +0200

    removeClass

--- www/scripts/run.js
+++ www/scripts/run.js
@@ -56,7 +56,7 @@ $("input[type=submit]").click(function(){
     else
     {
 	  $("#"+question).html('<img src="/images/gronbock.png" class="bock" />')
-	  $("#"+answers.question).removeclass('ui.draggable');
+	  $("#"+answers.question).removeClass('ui.draggable');
     }	
  		
 	if (allCorrect)
