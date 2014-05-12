6bf8cc401645ea043fa98e902d0043123ecd555f
MarcusBergman <marcus_bergman@hotmail.se>
2014-05-12 13:37:15 +0200

    Create deleteImage.php

---
+++ application/controllers/deleteImage.php
@@ -0,0 +1,22 @@
+<?php
+
+  $privateID = $matches[1];
+  $imageToBeDeleted = $matches[2];
+  
+  $test = getTestByPrivateID($privateID);
+  
+  $newArray = array();
+  
+  foreach($test["images"] as $i => $image)
+  {
+   if ($i != $imageToBeDeleted)
+     $newArray[] = $image;
+  }
+  
+  $test["images"] = $newArray;
+  
+  updateTestByPrivateID($privateID, $test);
+  
+  return;
+
+?>
