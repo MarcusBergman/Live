204ffdc243684b22d502d32efde69afa1b0db966
Mikael Tylmad <mikael@tylmad.com>
2014-05-13 09:56:25 +0200

    Update swapImages.php

--- application/controllers/swapImages.php
+++ application/controllers/swapImages.php
@@ -6,8 +6,6 @@
   
   $test = getTestByPrivateID($privateID);
   
-  if ()
-  
   $tempImage                    = $test['images'][$firstImage];
   $test['images'][$firstImage]  = $test['images'][$secondImage];
   $test['images'][$secondImage] = $tempImage;
