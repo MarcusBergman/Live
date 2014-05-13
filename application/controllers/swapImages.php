663baab05fe1121a8c8619ee55d5a2eca2829335
MarcusBergman <marcus_bergman@hotmail.se>
2014-05-13 11:30:21 +0200

    Update swapImages.php

--- application/controllers/swapImages.php
+++ application/controllers/swapImages.php
@@ -6,12 +6,23 @@
   
   $test = getTestByPrivateID($privateID);
   
+  $num = count($test["images"]);
+  
+  if($firstImage  >=0     && 
+     $firstImage  < $num &&
+     $secondImage <=0     &&
+     $secondImage < $num &&
+     $firstImage  != $secondImage
+     ) 
+  {
+  
   $tempImage                    = $test['images'][$firstImage];
   $test['images'][$firstImage]  = $test['images'][$secondImage];
   $test['images'][$secondImage] = $tempImage;
   
   updateTestByPrivateID($privateID, $test);
   
+  }
   return;
 
 ?>
