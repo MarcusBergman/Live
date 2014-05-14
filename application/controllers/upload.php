74f291471758db8b555d67190a3a252f10e48b0c
Bragge <trygglimurr@gmail.com>
2014-05-14 10:58:46 +0200

    print

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -7,9 +7,13 @@
   $uploads_dir = 'uploads';
   foreach ($_FILES["pictures"]["error"] as $key => $error) {
     if ($error == UPLOAD_ERR_OK) {
-        $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
+        print ("här är jag");
+		$tmp_name = $_FILES["pictures"]["tmp_name"][$key];
+		print ($tmp_name);
         $name = $_FILES["pictures"]["name"][$key];
+		print ($name);
         move_uploaded_file($tmp_name, "$uploads_dir/$publicID");
+		
     }
 }
 ?>
