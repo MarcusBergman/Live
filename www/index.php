326bc7f5e90dd299e609d73373d6036c44ef8f5f
Bragge <trygglimurr@gmail.com>
2014-05-14 10:11:08 +0200

    upload.php test

--- www/index.php
+++ www/index.php
@@ -14,7 +14,7 @@
    '/^bytBild\/([a-zA-Z]+)\/(\d+)\/(\d+)$/' => 'controllers/swapImages.php',
    '/^taBortBild\/([a-zA-Z]+)\/(\d+)$/'     => 'controllers/deleteImage.php',
    '/.*/'                                   => 'views/error.php.html',
-   '/^laddaUppBild\/([a-zA-Z]+)\/(\d+)$/'   => 'controllers/upload.php',
+   '/^laddaUppBild\/([a-zA-Z]+)$/'          => 'controllers/upload.php'
  );
  
  foreach ($routes as $regex => $route)
