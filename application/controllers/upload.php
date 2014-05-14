6e0c1cbf5d248edb966d9c6329384228fc8e5397
Bragge <trygglimurr@gmail.com>
2014-05-14 10:46:45 +0200

    Revert "public id igen"
    This reverts commit 04fe9f6d6668b309f5a1829957cdac4c5ccb84f8.

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -2,14 +2,13 @@
 
   $privateID = $matches[1];
   $test = getTestByPrivateID($privateID);
-  $publicID = getPublicIDByPrivateID($privateID);
 
   $uploads_dir = '/uploads';
   foreach ($_FILES["pictures"]["error"] as $key => $error) {
     if ($error == UPLOAD_ERR_OK) {
         $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
         $name = $_FILES["pictures"]["name"][$key];
-        move_uploaded_file($tmp_name, "$uploads_dir/$publicID");
+        move_uploaded_file($tmp_name, "$uploads_dir/$privateID");
     }
 }
 ?>
