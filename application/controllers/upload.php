e980ca3baf3bdac8f7799f375cfd348576e597b1
Bragge <trygglimurr@gmail.com>
2014-05-14 10:53:35 +0200

    /

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -2,13 +2,14 @@
 
   $privateID = $matches[1];
   $test = getTestByPrivateID($privateID);
+  $publicID = getPublicIDByPrivateID($privateID);
 
-  $uploads_dir = '/uploads';
+  $uploads_dir = 'uploads';
   foreach ($_FILES["pictures"]["error"] as $key => $error) {
     if ($error == UPLOAD_ERR_OK) {
         $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
         $name = $_FILES["pictures"]["name"][$key];
-        move_uploaded_file($tmp_name, "$uploads_dir/$privateID");
+        move_uploaded_file($tmp_name, "$uploads_dir/$publicID");
     }
 }
 ?>
