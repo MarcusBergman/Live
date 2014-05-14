1f47e0aa76eaf204af24ce1512305b6119d05c37
Bragge <trygglimurr@gmail.com>
2014-05-14 10:31:30 +0200

    foreach test

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -4,11 +4,11 @@
   $test = getTestByPrivateID($privateID);
 
   $uploads_dir = '/uploads';
-  $_FILES[]["error"] = $key => $error
-  if ($error == UPLOAD_ERR_OK)
-  {
-    $tmp_name = $_FILES[]["tmp_name"][$key];
-    $name = $_FILES[]["name"][$key];
-    move_uploaded_file($tmp_name, "$uploads_dir/$privateID");
-  }
+  foreach ($_FILES["pictures"]["error"] as $key => $error) {
+    if ($error == UPLOAD_ERR_OK) {
+        $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
+        $name = $_FILES["pictures"]["name"][$key];
+        move_uploaded_file($tmp_name, "$uploads_dir/$privateID");
+    }
+}
 ?>
