bc919cf22fa1f0d5a2da256391ed07dcb07d12c9
Bragge <trygglimurr@gmail.com>
2014-05-14 12:45:58 +0200

    return utan prints

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -5,21 +5,18 @@
   $publicID = getPublicIDByPrivateID($privateID);
 
   $uploads_dir = '/var/www/august.friskola.nu/Live/www/uploads';
-  print ("innan foreach");
-  print_r ($_FILES);
   
-    print ("innan if");
-	if ($_FILES["picture"]["error"] == UPLOAD_ERR_OK) {
-        print ("här är jag");
-		$tmp_name = $_FILES["picture"]["tmp_name"];
-		print ($tmp_name);
-        $name = $_FILES["picture"]["name"];
-		print ($name);
-        move_uploaded_file($tmp_name, "$uploads_dir/$publicID/$name");
+  if ($_FILES["picture"]["error"] == UPLOAD_ERR_OK) 
+  {
+    $tmp_name = $_FILES["picture"]["tmp_name"];
+    $name = $_FILES["picture"]["name"];
+    move_uploaded_file($tmp_name, "$uploads_dir/$publicID/$name");
 		
-		$test['images'][] = $name;
-		updateTestByPrivateID($privateID, $test);
+    $test['images'][] = $name;
+	updateTestByPrivateID($privateID, $test);
 		
-    }
+  }
+  
   return;
+  
 ?>
