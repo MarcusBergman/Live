63df954fc4ccfee58f329d604ee533d776abb00c
Bragge <trygglimurr@gmail.com>
2014-05-16 09:48:59 +0200

    bort med polyfill

--- www/scripts/home.js
+++ www/scripts/home.js
@@ -4,27 +4,13 @@ function redirect()
   window.location.href = "http://august.friskola.nu/testa/"+document.getElementById("testnamn").value
 
 }
-
-var polyfilter_scriptpath = '/scripts/filter_polyfill/'; 
-
-blur = function()
-{
-  $("#popup").css({"top": "30%", "opacity" : "1" });
-	  
-  $("#main").animate({ pixels: 8 },
-  {
-    step: function(now,fx) 
-    {
-      $(this).css('polyfilter','blur(' + Math.round(now) + 'px)'); 
-    },
-      duration: 500
-  },'swing');
-};
 		
-$("#starta").click(blur);
+$("#starta").click(function()
+{
+  $("#popup").css("top", "30%");
+});
   
 $("#kryss").click(function()
 {
   $("#popup").css("top","-100%");
-  $("#main").css("polyfilter",'blur(0px)');
 });
