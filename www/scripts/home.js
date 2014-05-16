a781b4da7b3b616171716a9156115d30631d9248
Bragge <trygglimurr@gmail.com>
2014-05-16 10:00:37 +0200

    testigera variable

--- www/scripts/home.js
+++ www/scripts/home.js
@@ -1,10 +1,21 @@
 function redirect()
 {
-  window.location.href = "http://august.friskola.nu/testa/"+document.getElementById("testnamn").value
+  var testigera = "";
+  
+  window.location.href = '"http://august.friskola.nu/'testigera'/"+document.getElementById("testnamn").value'
 }
 		
 $("#starta").click(function()
 {
+  testigera = "testa";
+  
+  $("#popup").css("top", "30%");
+});
+
+$("#redigera").click(function()
+{
+  testigera = "redigera";
+  
   $("#popup").css("top", "30%");
 });
   
