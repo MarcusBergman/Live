afa928cf6616bbe26186ac68dd54d2b04007511a
Bragge <trygglimurr@gmail.com>
2014-05-19 12:57:07 +0200

    event target

--- www/scripts/run.js
+++ www/scripts/run.js
@@ -39,7 +39,7 @@ $(function()
 
 $('.dragbild').on('dragstart', function (event)
 {
-  $(ui.draggable).attr("id").css({"width": $('.svar').width(), "height": $('.svar').width()});
+  $(event.target).attr("id").css({"width": $('.svar').width(), "height": $('.svar').width()});
 });
 	
 var answers = {};
