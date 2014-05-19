aaf0b005ddbba5a0523740104092543d2efeb04f
Bragge <trygglimurr@gmail.com>
2014-05-19 12:13:12 +0200

    .css width height

--- www/scripts/run.js
+++ www/scripts/run.js
@@ -21,6 +21,8 @@ for (var i=0; i<blandadeBilder.length; i++){
   $('#bilder').append('<div id="'+blandadeBilder[i].id+'" class="dragbild" style="background-image: url('+blandadeBilder[i].file+')"></div>');
 }
   
+$(".dragbild").css({"width": "24%", "height": "24%"});
+  
 var bredd =  84/bilder.length;
 
 $(".svar").css("width", bredd +"%");
