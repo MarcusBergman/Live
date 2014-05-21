418235af6d595d93fca5164758a958da77faaa90
Bragge <trygglimurr@gmail.com>
2014-05-21 10:56:45 +0200

    modelus

--- www/scripts/run.js
+++ www/scripts/run.js
@@ -21,13 +21,15 @@ for (var i=0; i<blandadeBilder.length; i++){
   $('#bilder').append('<div id="'+blandadeBilder[i].id+'" class="dragbild" style="background-image: url('+blandadeBilder[i].file+')"></div>');
   
   if (i <= 5)
-    $(blandadeBilder[i].id).css("top","45%");
+    $("#"+blandadeBilder[i].id).css("top","45%");
   else if (i > 5 && i <= 10)	
-    $(blandadeBilder[i].id).css("top","75%");
+    $("#"+blandadeBilder[i].id).css("top","75%");
   else if (i > 10 && i <= 15)	
-    $(blandadeBilder[i].id).css("top","105%");
+    $("#"+blandadeBilder[i].id).css("top","105%");
   else
-    $(blandadeBilder[i].id).css("top","135%");  
+    $("#"+blandadeBilder[i].id).css("top","135%");  
+	
+  $("#"+blandadeBilder[i].id).css("left", (i%5)*18+"%")	
 }
   
 var bredd =  84/bilder.length;
