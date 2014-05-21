7ec0aa3a7a3911cbaf7794ff2d2aa72b7369e085
Bragge <trygglimurr@gmail.com>
2014-05-21 11:04:24 +0200

    36 72 108

--- www/scripts/run.js
+++ www/scripts/run.js
@@ -23,11 +23,11 @@ for (var i=0; i<blandadeBilder.length; i++){
   if (i <= 5)
     $("#"+blandadeBilder[i].id).css("top","0%");
   else if (i > 5 && i <= 10)	
-    $("#"+blandadeBilder[i].id).css("top","40%");
+    $("#"+blandadeBilder[i].id).css("top","36%");
   else if (i > 10 && i <= 15)	
-    $("#"+blandadeBilder[i].id).css("top","80%");
+    $("#"+blandadeBilder[i].id).css("top","72%");
   else
-    $("#"+blandadeBilder[i].id).css("top","120%");  
+    $("#"+blandadeBilder[i].id).css("top","108%");  
 	
   $("#"+blandadeBilder[i].id).css("left", (i%5)*18+"%")	
 }
